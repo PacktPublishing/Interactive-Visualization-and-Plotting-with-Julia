8b565d5e77fdd7e01f8e4dfe1307756b22646eca
Diego Javier Zea <diegozea@gmail.com>
2022-02-02 22:07:04 +0100

    Update Chapter 3

--- Chapter03/Plots_plotly.jl
+++ Chapter03/Plots_plotly.jl
@@ -8,8 +8,8 @@ y_cos = cos.(x)
 plot(x, [y_sin y_cos], labels=["sin" "cos"])
 
 _round(number) = round(number, sigdigits=4)
-_point_label(x, y, fun) = "$fun($(_round(x))) = $(_round(y))"
-tooltips_sin = [_point_label(x, y, "sin") for (x, y) in zip(x, y_sin)]
-tooltips_cos = [_point_label(x, y, "cos") for (x, y) in zip(x, y_cos)]
+point_label(x, y, fun) = "$fun($(_round(x))) = $(_round(y))"
+tooltips_sin = [point_label(x, y, "sin") for (x, y) in zip(x, y_sin)]
+tooltips_cos = [point_label(x, y, "cos") for (x, y) in zip(x, y_cos)]
 
 plot(x, [y_sin y_cos], labels=["sin" "cos"], hover=[tooltips_sin tooltips_cos])
